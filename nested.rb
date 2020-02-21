92c5603499c7176a5aeea6d542ee7bf8004cab3d
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-21 04:13:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,7 +77,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {known_for: => "COBOL", languages: => ["COBOL", "FORTAN"]}
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
 end
 
