9dfc286bac68f9ce309e89689329afb9d7d5497d
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-21 04:04:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,9 +18,7 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
-	programmer_hash =
+		programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -35,6 +33,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
