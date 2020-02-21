6e5b9c50bb82929ddf427674cbd94e57f5951a07
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-21 04:14:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,6 @@ programmer_hash[:yukihiro_matsumoto] = {}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 programmer_hash
-
 end
 
 def changing_alan
